68cc2183972f03940cd3b7b9c2de2e90eb1cd4a3
criscarpio94 <carpiocris94@gmail.com>
2026-06-14 03:04:04 -0500

    "Pantalla de Ingreso con preview"

--- app/src/main/java/com/example/stockpro/PantallaIngreso.kt
+++ app/src/main/java/com/example/stockpro/PantallaIngreso.kt
@@ -1,2 +1,56 @@
 package com.example.stockpro
 
+import android.widget.Space
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun PantallaIngreso(controladorNavegacion : NavController) {
+    var nombre by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Bienvenido a StockPro", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+        Spacer(modifier = Modifier.height(24.dp))
+
+        TextField(value = nombre, onValueChange = { nombre = it }, label = { Text("Nombre de operario") })
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Button(
+            onClick = { controladorNavegacion.navigate("catalogo/$nombre") },
+            enabled = nombre.length >= 3
+        ) {
+            Text("Ingresar al Sistema")
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun PreviewPantallaIngreso() {
+    val controladorNavegacion = rememberNavController()
+    PantallaIngreso(controladorNavegacion)
+}
